e3b8d2ca5c6f5da9d1c26995c6db6f17c21c3960
Tamir <tamirphillips@live.com>
2023-12-01 18:01:44 -0500

    Added startercode, coded models, functionality for category routes.

---
+++ routes/api/category-routes.js
@@ -0,0 +1,66 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', async (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  console.log("Retrieving Data...");
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  console.log("Retrieving Data...");
+  try {
+    const categoryData = await Category.findByPk(req.params.id,{
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  // update a category by its `id` value
+  const updatedCategory = await Category.update(
+    {
+      // All the fields you can update and the data attached to the request body.
+      category_name: req.body.category_name,
+    },
+    {
+      // Gets a category based on the id given in the request parameters
+      where: {
+        id: req.params.id,
+      },
+    }
+  );
+  
+  res.json(updatedCategory);
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+});
+
+module.exports = router;
